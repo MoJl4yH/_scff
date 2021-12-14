3ba916c8dd4b100dba546e145f1ece218ad274c5
MoJl4yH <v.tkalin.ru@gmail.com>
2021-12-14 23:04:22 +0300

    little fix

--- scff.py
+++ scff.py
@@ -40,25 +40,33 @@ def FindComments(list_with_path_files, output_dir_name, regular_expression, regu
 
 
 # Фукнция выводящая колличество файлов по расширению
-def CountFile(count_file_in_path, count_analyse):
-    print(
-        f"Всего файлов в проекте = {count_file_in_path}. Проанализированно было {count_analyse}.")
+def CountFile(count_file_in_path, count_analyse, dict_with_entry):
+    dict_with_entry = {x: y for x, y in dict_with_entry.items() if y != 0}
+    print(f"Всего файлов в проекте = {count_file_in_path}. Проанализированно было {len(count_analyse)}.\n\
+В том числе файлы с расширением: {dict_with_entry}")
 
 
 # Функция осуществляющая поиск всех файлов в директории и в поддиректориях
 def FindFilesInPath(path):
     files_in_path = []  # заводим список куда будем писать все файлы
     all_files_count = 0  # переменная для хранения колличества всех файлов
+    dictEntry = {}  # словарь для анализа колличества проанализированных файлов по расширению
+    for i in extensions.list_ex:
+        dictEntry[i] = 0  # заполняем словарь информацией из списка
     # цикл поиска файлов в директории и поддиректории
     for folder, subfolders, files in os.walk(path):
         for f in files:
             all_files_count = all_files_count+1
-            file_Name, file_Extension = os.path.splitext(f)  # разбиваем файл на имя и расширение
+            file_Name, file_Extension = os.path.splitext(
+                f)  # разбиваем файл на имя и расширение
             for extension in extensions.list_ex:  # цикл, который ищет все файлы с расширением в list_ex
                 if file_Extension == extension and not (file_Name.startswith(".")):
                     # если файл не системный (не начинается с .) и подходит под расширение то добавляем его в спискок
-                    files_in_path.append(folder+"/"+f) #Добабвляем в конец списка путь до файла
-    return [files_in_path, all_files_count] #Возвращаем список с файлами и колличество всех файлов
+                    # Добабвляем в конец списка путь до файла
+                    files_in_path.append(folder+"/"+f)
+                    dictEntry[extension] = dictEntry[extension]+1
+    # Возвращаем список с файлами и колличество всех файлов
+    return [files_in_path, all_files_count, dictEntry]
 
 
 # ------Блок для аргументов коммандной строки-------
@@ -73,10 +81,12 @@ args = parser.parse_args()
 
 os.mkdir(args.o)  # Создаем папку в котором будут лежать отчеты о каждом ЯП
 
-# В ListWithPathFile лежат все найденные файлы с расширением из extensions.list_ex. В CountFileInPath колличество всех файлов в папке.
-ListWithPathFiles, CountFilesInPath = FindFilesInPath(os.path.abspath(args.i))
+# В ListWithPathFile лежат все найденные файлы с расширением из extensions.list_ex. В CountFileInPath колличество всех файлов в папке, в DictEntry словарь с колличеством проанализированных файлов по расширениям.
+ListWithPathFiles, CountFilesInPath, DictEntry = FindFilesInPath(
+    os.path.abspath(args.i))
 
 
 FindComments(ListWithPathFiles, args.o,
-             regular_expression, regular_expression_one) #Функция для поиска комментариев и записи их в файл
-CountFile(CountFilesInPath, len(ListWithPathFiles)) #Функция для вывода различной информации
+             regular_expression, regular_expression_one)  # Функция для поиска комментариев и записи их в файл
+# Функция для вывода различной информации
+CountFile(CountFilesInPath, ListWithPathFiles, DictEntry)
